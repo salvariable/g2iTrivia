76db577e691856b84b99764818127525d8c1a4d7
Salvador <45272829+salvariable@users.noreply.github.com>
2020-11-05 13:32:57 -0600

    Initial commit: test API and add file structure

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [trivia, setTrivia] = useState([]);
+
+  useEffect(() => {
+
+    fetch('https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean')
+    .then((response) => response.json())
+    .then((json) => {
+      setTrivia(json.results);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>
+        {trivia.length ? trivia.map((element) => element.question) : "Open up App.tsx to start working on your app!"}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
