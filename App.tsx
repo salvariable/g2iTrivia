b7b8bc7927d522a4960b40a166a7d866a1a4e02c
Salvador <45272829+salvariable@users.noreply.github.com>
2020-11-05 15:48:26 -0600

    Add reusable question component

--- App.tsx
+++ App.tsx
@@ -1,27 +1,11 @@
 import 'react-native-gesture-handler';
-import React, {useEffect, useState} from 'react';
-import { StyleSheet, View } from 'react-native';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './screens/Home';
 import Quiz from './screens/Quiz';
 
 export default function App() {
-
-  const [trivia, setTrivia] = useState([]);
-
-  useEffect(() => {
-
-    fetch('https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean')
-    .then((response) => response.json())
-    .then((json) => {
-      setTrivia(json.results);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-
-  }, [])
 
   const Stack = createStackNavigator();
 
@@ -30,17 +14,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="HomeScreen" component={Home} />
         <Stack.Screen name="QuizScreen" component={Quiz} />
-
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/Quiz.tsx
+++ screens/Quiz.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text} from "react-native";
+import Question from '../components/Question';
 
 export default () => {
+
+    const [trivia, setTrivia] = useState(null);
+
+    useEffect(() => {
+  
+      fetch('https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean')
+      .then((response) => response.json())
+      .then((json) => {
+          console.log("====== res", json.results)
+        setTrivia(json.results);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  
+    }, [])
+
+    
+
     return (
         <View style={{flex: 1, justifyContent: "space-around", alignItems: "center"}}>
-            <Text style={{fontWeight: "bold"}}>
-                This is a demo Quiz Screen
-            </Text>
+            {trivia && trivia.map((element, index) => <Question category={element.category} sentence={element.question} index={index + 1} triviaLength={trivia.length} /> )}
         </View>
     )
 }
