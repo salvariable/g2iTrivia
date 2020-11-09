62fcd13be688f4b8d83510fcb08b94b539e5a743
Salvador <45272829+salvariable@users.noreply.github.com>
2020-11-08 19:19:27 -0600

    Prevent gestures and header button from going back

--- App.tsx
+++ App.tsx
@@ -1,19 +1,26 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Home from './screens/Home';
-import Quiz from './screens/Quiz';
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./screens/Home";
+import Quiz from "./screens/Quiz";
+import Results from "./screens/Results";
 
 export default function App() {
-
   const Stack = createStackNavigator();
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="HomeScreen" component={Home} />
-        <Stack.Screen name="QuizScreen" component={Quiz} />
+      <Stack.Navigator
+        // Avoid enabling header and "back" gesture to prevent stack breaks
+        headerMode={"none"}
+        screenOptions={{
+          gestureEnabled: false,
+        }}
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Quiz" component={Quiz} />
+        <Stack.Screen name="Results" component={Results} />
       </Stack.Navigator>
     </NavigationContainer>
   );
