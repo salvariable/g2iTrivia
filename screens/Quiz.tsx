607c9a8825989825ce99a090379d800d841f230d
Salvador <45272829+salvariable@users.noreply.github.com>
2020-11-08 19:20:22 -0600

    Disable FlatList scroll and filter questions when responded

--- screens/Quiz.tsx
+++ screens/Quiz.tsx
@@ -1,30 +1,108 @@
-import React, {useEffect, useState} from 'react';
-import {View, Text} from "react-native";
-import Question from '../components/Question';
+import React, { useEffect, useState } from "react";
+import { FlatList, SafeAreaView, View, StyleSheet, Dimensions, Text } from "react-native";
+import BooleanButton from "../components/BooleanButton";
 
-export default () => {
+const Quiz = ({ navigation }) => {
+  const [trivia, setTrivia] = useState(null);
+  const [current, setCurrent] = useState(0);
+  const [results, setResults] = useState([]);
 
-    const [trivia, setTrivia] = useState(null);
-
-    useEffect(() => {
-  
-      fetch('https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean')
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean")
       .then((response) => response.json())
-      .then((json) => {
-          console.log("====== res", json.results)
-        setTrivia(json.results);
-      })
+      .then((json) => setTrivia(json.results))
       .catch((error) => {
         console.error(error);
       });
-  
-    }, [])
+  }, []);
 
-    
-
+  const renderQuestion = (item, index) => {
     return (
-        <View style={{flex: 1, justifyContent: "space-around", alignItems: "center"}}>
-            {trivia && trivia.map((element, index) => <Question category={element.category} sentence={element.question} index={index + 1} triviaLength={trivia.length} /> )}
+      <View style={styles.container} key={item}>
+        <Text style={styles.title}>{item.category}</Text>
+
+        <Text style={styles.question}>{item.question}</Text>
+
+        <Text key={item} style={styles.indicator}>
+          {index + 1} of {trivia.length}
+        </Text>
+
+        <View style={styles.buttonContainer}>
+          <BooleanButton
+            option={false}
+            onPress={() => saveResult(item.question, item.correct_answer, "False")}
+          />
+          <BooleanButton
+            option={true}
+            onPress={() => saveResult(item.question, item.correct_answer, "True")}
+          />
         </View>
-    )
-}
+      </View>
+    );
+  };
+
+  const saveResult = (question, correct, answer) => {
+    setResults((prevState) => {
+      const resultsArray = prevState;
+      resultsArray.push({ question, correct, answer });
+
+      setTrivia(trivia.filter((element) => question !== element.question));
+
+      if (results.length >= 10) {
+        return navigation.navigate("Results", { results: resultsArray });
+      }
+
+      return resultsArray;
+    });
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={trivia}
+        renderItem={({ item, index }) => renderQuestion(item, index)}
+        keyExtractor={(item) => item.question}
+        extraData={results}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        scrollEnabled={false}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  title: {
+    flex: 1,
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 24,
+    width: Dimensions.get("window").width - 16,
+  },
+  frame: { alignItems: "center" },
+  question: {
+    flex: 1,
+    fontSize: 24,
+    borderWidth: 1,
+    margin: 8,
+    textAlign: "center",
+    width: Dimensions.get("window").width - 16,
+  },
+  indicator: {
+    flex: 1,
+    alignSelf: "center",
+    fontSize: 24,
+    margin: 16,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+});
+export default Quiz;
