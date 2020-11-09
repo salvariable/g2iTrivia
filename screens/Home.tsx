15320162ff288a00ccf39674adba43465e3f769b
Salvador <45272829+salvariable@users.noreply.github.com>
2020-11-08 19:19:03 -0600

    Style Home component

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import {View, Text} from "react-native";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-export default ({navigation}) => {
-    return (
-        <View style={{flex: 1, justifyContent: "space-around", alignItems: "center"}}>
-            <Text style={{fontWeight: "bold"}}>
-                Welcome to the Trivia Challenge!
-            </Text>
-            <Text>
-                You will be presented with 10 True or False questions.
-            </Text>
-            <Text>
-                Can you score 100%?
-            </Text>
-            <Text onPress={() => navigation.navigate("QuizScreen")}>
-                BEGIN
-            </Text>
-        </View>
-    )
-}
+export default ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome to the Trivia Challenge!</Text>
+
+      <Text>You will be presented with 10 True or False questions.</Text>
+
+      <Text>Can you score 100%?</Text>
+
+      <Text onPress={() => navigation.navigate("Quiz")}>BEGIN</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  welcome: { fontWeight: "bold" },
+});
