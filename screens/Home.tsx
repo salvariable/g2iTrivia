bbd6b7e0bfbbce5ec5edb962c355f27902816761
Salvador <45272829+salvariable@users.noreply.github.com>
2020-11-12 01:24:12 -0600

    Adjust text styles at Home

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,17 +1,30 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, SafeAreaView } from "react-native";
 
-export default ({ navigation }) => {
+export interface Props {
+  navigation: any;
+}
+
+const Home: React.FC<Props> = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to the Trivia Challenge!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={[styles.welcome, styles.intro]}>
+        Welcome to the Trivia Challenge!
+      </Text>
 
-      <Text>You will be presented with 10 True or False questions.</Text>
+      <Text style={styles.intro}>
+        You will be presented with 10 True or False questions.
+      </Text>
 
-      <Text>Can you score 100%?</Text>
+      <Text style={styles.intro}>Can you score 100%?</Text>
 
-      <Text onPress={() => navigation.navigate("Quiz")}>BEGIN</Text>
-    </View>
+      <Text
+        style={styles.intro}
+        onPress={() => navigation.navigate("Quiz")}
+      >
+        BEGIN
+      </Text>
+    </SafeAreaView>
   );
 };
 
@@ -20,6 +33,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "space-around",
     alignItems: "center",
+    margin: 36,
   },
   welcome: { fontWeight: "bold" },
+  intro: {
+    fontSize: 24,
+    textAlign: "center",
+  },
 });
+
+export default Home;
